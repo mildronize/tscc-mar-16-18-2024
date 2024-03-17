f9ad5899036e8e9b67b6c0c05055748a64f55b13
Thada Wangthammang <thada.wth@gmail.com>
2024-03-17 21:16:34 +0700

    finish phase 1

--- api/src/modules/user/user.controller.ts
+++ api/src/modules/user/user.controller.ts
@@ -1,13 +1,60 @@
 import type { Request, Response } from 'express';
 import { UserRepository } from './user.repository';
+import { BaseController } from '@tscc/core';
 
-export class UserController {
+export class UserController extends BaseController {
 
-  constructor(public userRepository: UserRepository) {}
-
-  async getAll(req: Request, res: Response) {
-    return res.json(await this.userRepository.getAll());
+  constructor(public userRepository: UserRepository) {
+    super();
   }
+
+    /**
+   * Read a list of users
+   */
+    async getAll(req: Request, res: Response) {
+      return res.json({
+        data: await this.userRepository.getAll(),
+      });
+    }
+
+    /**
+     * Read a single user
+     */
+    async get(req: Request, res: Response) {
+      return res.json({
+        data: await this.userRepository.get(req.params.id),
+      });
+    }
+
+    /**
+     * Create a new user
+     */
+    async create(req: Request, res: Response) {
+      return res.json({
+        data: await this.userRepository.create(req.body),
+      });
+    }
+
+    /**
+     * Update a user
+     */
+    async update(req: Request, res: Response) {
+      return res.json({
+        data: await this.userRepository.update({
+          ...req.body,
+          id: req.params.id,
+        }),
+      });
+    }
+
+    /**
+     * Delete a user
+     */
+    async delete(req: Request, res: Response) {
+      return res.json({
+        data: await this.userRepository.delete(req.params.id),
+      });
+    }
 
 
 }
--- api/src/modules/user/user.repository.ts
+++ api/src/modules/user/user.repository.ts
@@ -9,4 +9,20 @@ export class UserRepository {
     return this.db.readAll();
   }
 
+  async get(id: string) {
+    return this.db.read(id);
+  }
+
+  async create(input: UserModel) {
+    return this.db.insert(input);
+  }
+
+  async update(input: UserModel) {
+    return this.db.update(input);
+  }
+
+  async delete(id: string) {
+    return this.db.delete(id);
+  }
+
 }
